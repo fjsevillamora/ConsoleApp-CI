d33961e639c765f9a4f17e347a9a2ca2ad91e8e9
Francisco Sevilla <fj.sevillamora@gmail.com>
2022-11-15 09:46:09 -0600

    Intentionally error

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp_CI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Demo .NET Framework 4.6.1 Testing");
+            Console.WriteLine("Demo .NET Framework 4.6.1 Testing")
             Console.ReadLine();
         }
     }
