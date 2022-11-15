496c586d08be0112877c1a0b26e2fc4d69a36d3d
Francisco Sevilla <fj.sevillamora@gmail.com>
2022-11-15 09:42:23 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp_CI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo .NET Framework 4.6.1 Testing");
+            Console.ReadLine();
+        }
+    }
+}
